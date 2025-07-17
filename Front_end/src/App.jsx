9841892ebf58c6e8c6f844722adfc698d8f32f67
Fitsum1101 <fitsumt835@gmail.com>
2025-07-17 06:42:53 +0300

    installing react and tailwindCss

---
+++ Front_end/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-2xl mx-auto bg-yellow-950 mt-10 ring-2 ring-amber-400">
+      <h1>Hello, Vite + React!</h1>
+      <p>This is your restored App component.</p>
+    </div>
+  );
+}
+
+export default App;
