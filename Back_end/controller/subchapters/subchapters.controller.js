f940aced5d8c0dbade2be4cdb8c977acb78fd424
Fitsum1101 <fitsumt835@gmail.com>
2025-07-22 22:48:11 +0300

    subchapter controller started with out testing it work

--- Back_end/controller/subchapters/subchapters.controller.js
+++ Back_end/controller/subchapters/subchapters.controller.js
@@ -1,18 +1,56 @@
+const db = require("../../config/db");
+const cloudinary = require("../../config/coudinary");
+const validate = require("../../util/validate.util");
+const { deleteFile } = require("../../util/removeFile");
+
 exports.createSubChapter = async (req, res, next) => {
   try {
-    const { title, videoUrl, videoLength, order, chapterId } = req.body;
+    const existingFields = validate(req);
+    const isFileUploaded = typeof req.file === "object";
+
+    if (Object.keys(existingFields).length > 0) {
+      res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        errors: existingFields,
+      });
+      return isFileUploaded && deleteFile(require.main.path, req, file.path);
+    }
+
+    const { title, order, chapterId } = req.body;
+
+    const chapter = await db.chapter.findFirst({
+      where: { id: chapterId },
+    });
+
+    const file = req.file;
+    const filePath = path.join(require.main.path, file.filename);
+    const fileName = file.filename + file.mimetype.split("/")[1];
+    const folderFileName = "course//" + chapter.title + "//file" + fileName;
+
+    const result = await cloudinary.uploader.upload(filePath, {
+      public_id: folderFileName,
+      resource_type: "auto",
+      use_filename: true,
+      unique_filename: false,
+      overwrite: false,
+    });
+
+    const fileURL = result.secure_url;
 
     const subChapter = await prisma.subChapter.create({
       data: {
         title,
-        videoUrl,
-        videoLength,
+        videoUrl: fileURL,
         order,
         chapterId,
       },
     });
 
-    res.status(201).json(subChapter);
+    res.status(201).json({
+      success: true,
+      subChapter,
+    });
   } catch (err) {
     next(err);
   }
