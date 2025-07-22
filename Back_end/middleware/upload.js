44cd128c5a13befb54b9a342588eaae71c461874
Fitsum1101 <fitsumt835@gmail.com>
2025-07-22 22:49:09 +0300

    file validation added

--- Back_end/middleware/upload.js
+++ Back_end/middleware/upload.js
@@ -2,13 +2,15 @@ const multer = require("multer");
 const path = require("path");
 const createFolder = require("../util/foldecreate");
 
-const fileUploadHandler = (folderName) => {
+const fileUploadHandler = (folderName, fileTypeCol) => {
   createFolder(folderName);
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, folderName);
     },
     filename: function (req, file, cb) {
+      if (!fileTypeCol.incudes(file.mimetype.split("/")[1]))
+        return cb(null, false);
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
       cb(null, uniqueSuffix + path.extname(file.originalname));
     },
