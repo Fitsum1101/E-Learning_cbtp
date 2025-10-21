1d6133c11fdbab890b488fbfd4ead4e10a082c10
Fitsum1101 <fitsumt835@gmail.com>
2025-10-22 01:07:59 +0300

    home page is done

---
+++ Front_end/src/pages/main/Home.jsx
@@ -0,0 +1,15 @@
+import FeaturedCourses from "../../components/layouts/Feachers";
+import AvatarsCertificates from "../../components/layouts/Avater";
+import HeroSection from "../../components/layouts/Hero";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeaturedCourses />
+      <AvatarsCertificates />
+    </>
+  );
+};
+
+export default HomePage;
